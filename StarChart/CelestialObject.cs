04e4b22a6cb051fe04a60dad00122c5c23da50ec
Allyssa Batten <abatten@truefit.io>
2019-09-04 08:45:18 -0400

    Create CelestialObjectController's Get Actions

--- StarChart/CelestialObject.cs
+++
@@ -1,7 +0,0 @@
-namespace StarChart
-{
-    public class CelestialObject
-    {
-        
-    }
-}
--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -8,11 +8,13 @@ namespace StarChart.Models
     public class CelestialObject
     {
         public int Id { get; set; }
+        
         [Required]
         public string Name { get; set; }
 
         public int? OrbitedObjectId { get; set; }
-        [NotMapped]
+
+        [NotMapped] 
         public List<CelestialObject> Satellites { get; set; }
 
         public TimeSpan OrbitalPeriod { get; set; }
